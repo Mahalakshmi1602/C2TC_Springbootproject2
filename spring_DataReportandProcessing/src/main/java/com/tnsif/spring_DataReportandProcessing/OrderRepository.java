019f71dbe06858481c907266072ae3379df6c2b7
Mahalakshmi Manoharan <mahalakshmi.manohar2002@gmail.com>
2025-08-16 23:31:07 +0530

    This is a program

---
+++ spring_DataReportandProcessing/src/main/java/com/tnsif/spring_DataReportandProcessing/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.tnsif.spring_DataReportandProcessing;
+
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
+
